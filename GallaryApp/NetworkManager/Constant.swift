5b143942165f51313c870ebb2a1f9c80a9202b8a
Ahmed Hamdy <dimo.hamdy@gmail.com>
2018-09-07 06:39:45 +0200

    add warnings to change IP

--- GallaryApp/NetworkManager/Constant.swift
+++ GallaryApp/NetworkManager/Constant.swift
@@ -11,12 +11,11 @@ import Foundation
 enum API : String {
     case upload = "/user/fileUpload"
     case files = "/user/files"
-//    case image(String) =
     
     var url:String {
         return API.domain + self.rawValue
     }
-
+    //FIXME:Please change ip address of the server
     static let domain:String = "http://192.168.1.3:8080"
 
 }
